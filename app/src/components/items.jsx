d3cb0da2b2bab0932d60554c4c95233bcdf473ea
alinivan-solirius <alin.ivan@solirius.com>
2023-10-02 13:59:14 +0100

    Added forms

--- app/src/components/items.jsx
+++ app/src/components/items.jsx
@@ -11,6 +11,7 @@ import { Link } from 'react-router-dom';
 import card_holder_img from "../resources/holder.png";
 import {CardBody, CloseButton, Modal} from "react-bootstrap";
 import * as Icon from 'react-bootstrap-icons';
+import Form from "react-bootstrap/Form";
 
 
 const Items = () =>{
@@ -22,10 +23,10 @@ const Items = () =>{
     const [showDelete, setShowDelete] = useState(false);
     const [showEdit, setShowEdit] = useState(false);
 
-    const handleCloseDeleteModal = () => setShowDelete(false);
-    const handleShowDeleteModal = () => setShowDelete(true);
     const handleCloseEditModal = () => setShowEdit(false);
     const handleShowEditModal = () => setShowEdit(true);
+    const handleCloseDeleteModal = () => setShowDelete(false);
+    const handleShowDeleteModal = () => setShowDelete(true);
 
     const getItems = () => {
         axios.get('http://localhost:8080/api/item')
@@ -39,9 +40,15 @@ const Items = () =>{
             });
     }
 
+    const deleteItem = (e) => {
+        console.log(e.target);
+        handleCloseDeleteModal();
+    }
+
     useEffect(() => {
         getItems();
-    }, []);
+    }, [deleteItem]);
+
 
     return (
         <div className="background">
@@ -50,8 +57,8 @@ const Items = () =>{
                     <h1 className='landing-text'style = {{padding:'20px'}}>Items </h1>
                     <div>
                         <Row>
-                            {items.map((item) => (
-                                <Col lg={4} md={6} xs={12} key={item.id}>
+                            {items.map((item, i) => (
+                                <Col lg={4} md={6} xs={12} key={i}>
                                     <Card variant="dark" style={{ margin: '0.2rem', backgroundColor: '#1e1e1f' , color: '#d7d7d9'}}>
                                         <Card.Img variant="top" src={card_holder_img}/>
                                         <Card.Body  style={{position:"absolute", top:"0", right:"0"}}>
@@ -59,7 +66,7 @@ const Items = () =>{
                                                 <Icon.PencilSquare onClick={handleShowEditModal} size={30}/>
                                             </span>
                                             <span className="delete-button">
-                                                 <Icon.XCircle onClick={handleShowDeleteModal} size={30} style={{marginLeft:".5rem"}}/>
+                                                 <Icon.XCircle key={i} onClick={handleShowDeleteModal} size={30} style={{marginLeft:".5rem"}}/>
                                             </span>
                                         </Card.Body>
                                         <Card.Body>
@@ -74,39 +81,69 @@ const Items = () =>{
                                 </Col>
                             ))}
                         </Row>
+                        <Modal show={showEdit} onHide={handleCloseEditModal}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Update item</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form noValidate>
+                                    <Row className="mb-3">
+                                        <Form.Group as={Col} md="12" controlId="validationCustom01">
+                                            <Form.Label style={{fontWeight:"bold"}}>Item title</Form.Label>
+                                            <Form.Control  style={{padding:"1rem", marginBottom:"1rem"}}
+                                                           required
+                                                           type="text"
+                                                           placeholder="Title.."
+                                            />
+                                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group as={Col} md="12" controlId="validationCustom02">
+                                            <Form.Label  style={{fontWeight:"bold"}}>URL</Form.Label>
+                                            <Form.Control  style={{padding:"1rem", marginBottom:"1rem"}}
+                                                           required
+                                                           type="text"
+                                                           placeholder="URL.."
+                                            />
+                                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group as={Col} md="12" controlId="exampleForm.ControlTextarea1">
+                                            <Form.Label  style={{fontWeight:"bold"}}>Description</Form.Label>
+                                            <Form.Control style={{padding:"1rem", marginBottom:"1rem"}}
+                                                          required
+                                                          as="textarea" rows={10}
+                                                          type="text"
+                                                          placeholder="Description.."
+                                            />
+                                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                        </Form.Group>
+                                    </Row>
+                                </Form>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="light" onClick={handleCloseEditModal}>Close</Button>
+                                <Button variant="dark" onClick={handleCloseEditModal} style={{padding: "0.8rem"}} type="submit">Save changes</Button>
+                            </Modal.Footer>
+                        </Modal>
+
+                        <Modal show={showDelete} onHide={handleCloseDeleteModal}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Delete item</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>Are you sure you want to delete this item?</Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="light" onClick={handleCloseDeleteModal}>
+                                    Close
+                                </Button>
+                                <Button variant="dark" onClick={deleteItem}>
+                                    Delete
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
                     </div>
                 </div>
             </Container>
 
-            <Modal show={showEdit} onHide={handleCloseEditModal}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Update item</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>To be added</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="light" onClick={handleCloseEditModal}>
-                        Close
-                    </Button>
-                    <Button variant="dark" onClick={handleCloseEditModal}>
-                        Save changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
 
-            <Modal show={showDelete} onHide={handleCloseDeleteModal}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Delete item</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Are you sure you want to delete this item?</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="light" onClick={handleCloseDeleteModal}>
-                        Close
-                    </Button>
-                    <Button variant="dark" onClick={handleCloseDeleteModal}>
-                        Delete
-                    </Button>
-                </Modal.Footer>
-            </Modal>
 
         </div >
     )
--- app/src/pages/add.jsx
+++ app/src/pages/add.jsx
@@ -1,10 +1,68 @@
+import Container from "react-bootstrap/Container";
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
 
 const Add = () =>{
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
     return (
-        <div className="wrapper">
-            Add
+        <div className="background">
+            <Container>
+                <div className="bgBlur">
+                    <h1 className='landing-text'style = {{padding:'20px'}}>Add new item </h1>
+                    <div>
+                        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                            <Row className="mb-3">
+                                <Form.Group as={Col} md="6" controlId="validationCustom01">
+                                    <Form.Label style={{fontWeight:"bold"}}>Item title</Form.Label>
+                                    <Form.Control  style={{padding:"1rem", marginBottom:"1rem"}}
+                                        required
+                                        type="text"
+                                        placeholder="Title.."
+                                    />
+                                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group as={Col} md="6" controlId="validationCustom02">
+                                    <Form.Label  style={{fontWeight:"bold"}}>URL</Form.Label>
+                                    <Form.Control  style={{padding:"1rem", marginBottom:"1rem"}}
+                                        required
+                                        type="text"
+                                        placeholder="URL.."
+                                    />
+                                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group as={Col} md="12" controlId="exampleForm.ControlTextarea1">
+                                    <Form.Label  style={{fontWeight:"bold"}}>Description</Form.Label>
+                                    <Form.Control style={{padding:"1rem", marginBottom:"1rem"}}
+                                                  required
+                                                  as="textarea" rows={10}
+                                                  type="text"
+                                                  placeholder="Description.."
+                                    />
+                                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                </Form.Group>
+                            </Row>
+                            <Button variant="dark" style={{padding: "0.8rem"}} type="submit">Submit form</Button>
+                        </Form>
+                    </div>
+                </div>
+            </Container>
         </div>
-    )
+    );
 }
 
 export default Add;
