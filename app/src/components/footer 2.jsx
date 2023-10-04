5572bfdc2a0d38e84714ad1b42a45e07faa2b632
alinivan-solirius <alin.ivan@solirius.com>
2023-10-04 15:02:21 +0100

    Added item-image config

--- app/src/components/footer 2.jsx
+++
@@ -1,9 +0,0 @@
-
-const Footer = () => {
-    return (
-        <div className="wrapper" style={{minHeight: "200px", backgroundColor:"#202224", marginTop:"1rem"}}>
-
-        </div>
-    )
-}
-export default Footer;
